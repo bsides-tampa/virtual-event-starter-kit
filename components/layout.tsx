983c1b96892536e05baef63c076d067539b5b678
Joshua Grose <jgrose@gmail.com>
2021-11-13 14:43:37 -0500

    commented out line 26 - view-source

--- components/layout.tsx
+++ components/layout.tsx
@@ -23,7 +23,7 @@ import styles from './layout.module.css';
 import Logo from './icons/icon-logo';
 import MobileMenu from './mobile-menu';
 import Footer, { HostedByVercel } from './footer';
-import ViewSource from '@components/view-source';
+/* import ViewSource from '@components/view-source'; */
 
 type Props = {
   children: React.ReactNode;
